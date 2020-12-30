390de41fcfc195cd5bccae3d7c8dc41ace85f00f
Rithvik <rithvik7@illinois.edu>
2020-12-30 04:41:18 -0600

    Trying to center elements.

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,28 +1,24 @@
-import  { Button, Form } from 'react-bootstrap';
-import Template from './Template'
+import  { Col, Container, Form, Row } from 'react-bootstrap';
 import './styles/styles.css'
 
 export default function Popup() {
 
   return (
     <div className='popup'>
-      <Form>
-
-        <Form.Group>
-          <Form.Label>Job Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter your title." />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Company</Form.Label>
-          <Form.Control type="text" placeholder="Enter your company." />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-
-      </Form>
+      <Container>
+        <Row>
+          <Col />
+          <Col>
+            <Form>
+              <Form.Group>
+                <Form.Label>Job Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter your title." />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </div>
   )
 }
